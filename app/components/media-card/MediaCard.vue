e021437bba4843fda483eaf33eb0fb52ed2fdf30
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-05-20 20:22:18 +0300

    create media card content

--- app/components/media-card/MediaCard.vue
+++ app/components/media-card/MediaCard.vue
@@ -9,12 +9,18 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div class="media-card" :style="height ? { height: `${height}px` } : {}">
-    <img :src="imageUrl" :alt="text" class="media-card__image" />
-
-    <div class="media-card__content">
-      <p class="media-card__text">{{ text }}</p>
+  <div
+    class="media-card"
+    :style="{ width: `${width}px`, height: `${height}px` }"
+  >
+    <div class="media-card__player player">
+      <button class="player__play-button">
+        <UiIcon name="play" class="player__play-button-icon" />
+      </button>
+      <div class="player__progress-bar">progress bar</div>
     </div>
+    <p class="media-card__text">{{ text }}</p>
+    <img :src="imageUrl" :alt="text" class="media-card__image" />
   </div>
 </template>
 
@@ -25,6 +31,64 @@ defineProps<Props>();
   height: 100%;
   overflow: hidden;
   border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 12px;
+
+  container-type: size;
+
+  // &__container {
+  //   container-type: inline-size;
+  // }
+
+  .player {
+    display: flex;
+    gap: 15px;
+    align-items: center;
+
+    @container (height <= 118px) {
+      gap: 9px;
+    }
+    @container (118px < height <= 170px) {
+      gap: 13px;
+    }
+
+    &__play-button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 8px;
+      width: 38px;
+      height: 30px;
+      background-color: rgba(0, 0, 0, 0.6);
+      cursor: pointer;
+      transition: background-color 0.2s ease-in-out;
+
+      @container (height <= 118px) {
+        width: 23px;
+        height: 18px;
+        border-radius: 5px;
+
+        .player__play-button-icon {
+          width: 7px;
+          height: 8px;
+        }
+      }
+
+      @container (118px < height <= 170px) {
+        width: 32px;
+        height: 26px;
+        border-radius: 6px;
+      }
+
+      @media (hover: hover) {
+        &:hover {
+          background-color: rgba(0, 0, 0, 1);
+        }
+      }
+    }
+  }
 
   &__image {
     position: absolute;
@@ -34,6 +98,24 @@ defineProps<Props>();
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+
+  &__text {
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 13px;
+    color: var(--4);
+    margin-top: 8px;
+
+    @container (height <= 118px) {
+      font-size: 9px;
+      margin-top: 5px;
+    }
+
+    @container (118px < height <= 170px) {
+      font-size: 11px;
+      margin-top: 7px;
+    }
   }
 }
 </style>
--- app/components/media-masonry/MediaMasonry.vue
+++ app/components/media-masonry/MediaMasonry.vue
@@ -9,111 +9,115 @@ const cards = ref([
     text: "Amet minim mollit non deserunt ullamco",
     img: "https://images.unsplash.com/photo-1545205597-3d9d02c29597?q=80&w=500",
     height: 118,
+    width: 211,
   },
   {
     id: 2,
     text: "Amet minim mollit non deserunt",
     img: "https://images.unsplash.com/photo-1518611012118-696072aa579a?q=80&w=500",
     height: 170,
+    width: 213,
   },
   {
     id: 3,
     text: "Amet minim mollit non deserunt",
     img: "https://images.unsplash.com/photo-1506126613408-eca07ce68773?q=80&w=500",
     height: 200,
+    width: 354,
   },
   {
     id: 4,
     text: "Amet minim mollit non deserunt ullamco",
     img: "https://images.unsplash.com/photo-1517838277536-f5f99be501cd?q=80&w=500",
     height: 116,
+    width: 207,
   },
   {
     id: 5,
     text: "Amet minim mollit non deserunt ullamco",
     img: "https://images.unsplash.com/photo-1594381898411-846e7d193883?q=80&w=500",
     height: 160,
+    width: 199,
   },
   {
     id: 6,
     text: "Amet minim mollit non deserunt",
     img: "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=500",
     height: 200,
+    width: 351,
   },
   {
     id: 7,
     text: "Amet minim mollit non deserunt ullamco",
     img: "https://images.unsplash.com/photo-1517838277536-f5f99be501cd?q=80&w=500",
     height: 170,
+    width: 211,
   },
   {
     id: 8,
     text: "Amet minim mollit non deserunt ullamco",
     img: "https://images.unsplash.com/photo-1594381898411-846e7d193883?q=80&w=500",
     height: 118,
+    width: 207,
   },
 ]);
 
-onMounted(() => {
-  if (!masonryContainer.value) return;
+// onMounted(() => {
+//   if (!masonryContainer.value) return;
 
-  const cardElements = masonryContainer.value.querySelectorAll(
-    ".media-masonry__card",
-  );
+//   const cardElements = masonryContainer.value.querySelectorAll(
+//     ".media-masonry__card",
+//   );
 
-  cardElements.forEach((card) => {
-    const durationX = 3 + Math.random() * 2.5;
-    const durationY = 3.5 + Math.random() * 2.5;
-    const moveX = 4 + Math.random() * 4;
-    const moveY = 3 + Math.random() * 4;
+//   cardElements.forEach((card) => {
+//     const durationX = 3 + Math.random() * 2.5;
+//     const durationY = 3.5 + Math.random() * 2.5;
+//     const moveX = 4 + Math.random() * 4;
+//     const moveY = 3 + Math.random() * 4;
 
-    const tweens: gsap.core.Tween[] = [];
+//     const tweens: gsap.core.Tween[] = [];
 
-    // Анимация покачивания по оси X
-    tweens.push(
-      gsap.to(card, {
-        x: moveX,
-        duration: durationX,
-        repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut",
-      }),
-    );
+//     tweens.push(
+//       gsap.to(card, {
+//         x: moveX,
+//         duration: durationX,
+//         repeat: -1,
+//         yoyo: true,
+//         ease: "sine.inOut",
+//       }),
+//     );
 
-    // Анимация покачивания по оси Y
-    tweens.push(
-      gsap.to(card, {
-        y: moveY,
-        duration: durationY,
-        repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut",
-      }),
-    );
+//     tweens.push(
+//       gsap.to(card, {
+//         y: moveY,
+//         duration: durationY,
+//         repeat: -1,
+//         yoyo: true,
+//         ease: "sine.inOut",
+//       }),
+//     );
 
-    // Пауза движения + плавное увеличение масштаба (scale) через GSAP
-    card.addEventListener("mouseenter", () => {
-      tweens.forEach((tween) => tween.pause());
-      gsap.to(card, {
-        scale: 1.01,
-        rotateZ: 0.01 /* Фикс размытия текста */,
-        duration: 0.4,
-        ease: "power2.out",
-      });
-    });
+//     card.addEventListener("mouseenter", () => {
+//       tweens.forEach((tween) => tween.pause());
+//       gsap.to(card, {
+//         scale: 1.01,
+//         rotateZ: 0.01,
+//         duration: 0.4,
+//         ease: "power2.out",
+//       });
+//     });
 
-    // Возобновление движения + плавный возврат масштаба к исходному
-    card.addEventListener("mouseleave", () => {
-      tweens.forEach((tween) => tween.play());
-      gsap.to(card, {
-        scale: 1,
-        rotateZ: 0,
-        duration: 0.4,
-        ease: "power2.out",
-      });
-    });
-  });
-});
+//     card.addEventListener("mouseleave", () => {
+//       tweens.forEach((tween) => tween.play());
+//       gsap.to(card, {
+//         scale: 1,
+//         rotateZ: 0,
+//         duration: 0.4,
+//         ease: "power2.out",
+//       });
+//     });
+//   });
+// });
 </script>
 
 <template>
@@ -125,6 +129,7 @@ onMounted(() => {
         :image-url="card.img"
         :text="card.text"
         :height="card.height"
+        :width="card.width"
         :class="`media-masonry__card media-masonry__card_${index + 1}`"
       />
     </div>
@@ -163,7 +168,6 @@ onMounted(() => {
     &:hover {
       z-index: 10;
 
-      /* Равномерная контурная тень: выходит ровно на 10px во все стороны (spread = 10px) */
       box-shadow: 0 0 15px 5px rgba(187, 187, 187, 0.35);
     }
 
