f989a6b706c9af6a515a68a9bfb4d08d2cd948b2
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-05-21 03:14:56 +0300

    create auth page

--- app/pages/auth/index.vue
+++ app/pages/auth/index.vue
@@ -6,12 +6,23 @@ definePageMeta({
 <template>
   <div class="auth-page">
     <div class="auth-page__links">
-      <UiButton as="link" to="/auth/creator/signin" variant="outline">
-        Creator</UiButton
-      >
-      <UiButton as="link" to="/auth/viewer/signin" variant="outline">
-        Viewer</UiButton
-      >
+      <NuxtLink to="/auth/creator/signin" class="auth-page__creator">
+        <NuxtImg
+          src="https://images.unsplash.com/photo-1548690312-e3b507d8c110?q=80&w=1900&auto=format"
+          alt="logo"
+          class="auth-page__creator-img auth-page__img"
+        />
+        <UiButton variant="outline" class="auth-page__btn"> Creator</UiButton>
+      </NuxtLink>
+
+      <NuxtLink to="/auth/viewer/signin" class="auth-page__viewer">
+        <NuxtImg
+          src="https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?q=80&w=1900&auto=format"
+          alt="logo"
+          class="auth-page__viewer-img auth-page__img"
+        />
+        <UiButton variant="outline" class="auth-page__btn"> Viewer</UiButton>
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -22,10 +33,53 @@ definePageMeta({
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 
   &__links {
-    display: flex;
-    gap: 20px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    width: 100%;
+    height: 100%;
+
+    @media (max-width: 767px) {
+      height: auto;
+      max-height: 100vh;
+    }
+  }
+
+  &__creator,
+  &__viewer {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+    cursor: pointer;
+
+    @media (max-width: 767px) {
+      height: auto;
+    }
+  }
+
+  &__img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: 50% 50%;
+  }
+
+  &__btn {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    width: 100%;
+    color: var(--3);
+    font-size: globalFunctions.fluidValue(18px, 48px, 375px, 1440px);
+
+    background: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    border: none;
   }
 }
 </style>
