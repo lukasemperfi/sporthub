0b999c677414ca3938b7390eb049bb3d8e37dd5c
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-05-21 14:40:48 +0300

    connect signin api

--- app/pages/auth/creator/SignIn.vue
+++ app/pages/auth/creator/SignIn.vue
@@ -5,14 +5,29 @@ definePageMeta({
   layout: "auth-creator",
 });
 
-const onSubmit: (
+const authApi = useAuthApi();
+const router = useRouter();
+const isLoading = ref(false);
+
+const handleSignIn: (
   values: GenericObject,
   actions: SubmissionContext<GenericObject>,
-) => void = (values, actions) => {
-  console.log(values, actions);
+) => void = async ({ values, actions }) => {
+  isLoading.value = true;
+  try {
+    await authApi.signIn(values.email, values.password);
+
+    router.push("/");
+  } catch (err: any) {
+    console.error(err);
+    actions.setErrors({ password: "Uncorrect email or password" });
+  } finally {
+    isLoading.value = false;
+  }
 };
 </script>
 <template>
-  <AuthSignInForm @submit="onSubmit" user-role="creator" />
+  <AuthSignInForm @submit="handleSignIn" user-role="creator" />
+  <UiLoadingOverlay v-if="isLoading" />
 </template>
 <style lang="scss" scoped></style>
